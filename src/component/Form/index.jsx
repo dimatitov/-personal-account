89cbde197b55e5b3d7a9f663420832b4e58129fc
Dmitruy Titov <titovdima95@gmail.com>
2020-09-15 05:16:38 +0500

    test prettier 1

--- src/component/Form/index.jsx
+++ src/component/Form/index.jsx
@@ -1,66 +1,61 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-
-
 import './Form.css'
 
 const Form = ({ onAddContact }) => {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
 
-   const [name, setName] = useState('')
-   const [phone, setPhone] = useState('')
+  const handleSubmit = (event) => {
+    event.preventDefault()
 
-   const handleSubmit = (event) => {
-      event.preventDefault()
+    if (name && phone) {
+      onAddContact(name, phone)
+      setPhone('')
+      setName('')
+    }
+  }
 
-      if(name && phone) {
-         onAddContact(name, phone)
-         setPhone('')
-         setName('')
-      }
-   }
+  const handleChangeName = (event) => {
+    let name = event.target.value
+    setName(name)
+  }
 
-   const handleChangeName = (event) => {
-      let name = event.target.value
-      setName(name)
-   }
+  const handleChangePhone = (event) => {
+    let phone = event.target.value
+    setPhone(phone)
+  }
 
-   const handleChangePhone = (event) => {
-      let phone = event.target.value
-      setPhone(phone)
-   }
+  return (
+    <section>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="form-container">
+          <input
+            type="text"
+            placeholder="Name"
+            className="form-name"
+            value={name}
+            onChange={handleChangeName}
+          />
+          <input
+            type="text"
+            placeholder="Phone"
+            value={phone}
+            onChange={handleChangePhone}
+          />
+        </div>
 
-   return (
-      <section>
-         <form className="contact-form" onSubmit={handleSubmit}>
-            <div className="form-container">
-               <input
-                   type="text"
-                   placeholder="Name"
-                   className="form-name"
-                   value={name}
-                   onChange={handleChangeName}
-               />
-               <input
-                   type="text"
-                   placeholder="Phone"
-                   value={phone}
-                   onChange={handleChangePhone}
-               />
-            </div>
-            
-            <button type="submit" className="form-btn">
-               <i className="material-icons">add</i>
-            </button>
-         </form>
-
-         
-      </section>
-   )
+        <button type="submit" className="form-btn">
+          <i className="material-icons">add</i>
+        </button>
+      </form>
+    </section>
+  )
 }
 
 Form.propTypes = {
-   onAddContact: PropTypes.func.isRequired
+  onAddContact: PropTypes.func.isRequired,
 }
 
 export default Form
