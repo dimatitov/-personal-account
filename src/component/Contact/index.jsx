904d60e76b7e9f814095e67b634dadb9f52a4d11
Dmitruy Titov <titovdima95@gmail.com>
2020-09-15 05:15:08 +0500

    test prettier

--- src/component/Contact/index.jsx
+++ src/component/Contact/index.jsx
@@ -1,58 +1,59 @@
 import React, { useContext, useState } from 'react'
 import ContactView from '../ContactView'
 import Form from '../Form'
-import Context from "../../context";
+import Context from '../../context'
 import contacts from '../../contacts'
 
-
 import './Contact.css'
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
-    const { user } = useContext(Context)
-    const [contactList, setContactList] = useState(contacts)
+  const { user } = useContext(Context)
+  const [contactList, setContactList] = useState(contacts)
 
-    const handleEdit = (id, name, phone) => {
-        setContactList(contactList => contactList.map(contact =>
-            contact.id === id
-                ? { ...contact, name, phone }
-                : contact)
-        )
-    }
+  const handleEdit = (id, name, phone) => {
+    setContactList((contactList) =>
+      contactList.map((contact) =>
+        contact.id === id ? { ...contact, name, phone } : contact,
+      ),
+    )
+  }
 
-    const handleAddContact = (name, phone) => {
-        setContactList(contactList.concat([
-            {
-                id: Date.now(),
-                name,
-                phone
-            },
-        ]))
-    }
+  const handleAddContact = (name, phone) => {
+    setContactList(
+      contactList.concat([
+        {
+          id: Date.now(),
+          name,
+          phone,
+        },
+      ]),
+    )
+  }
 
-    const handleDelete = (id) => {
-        setContactList(contactList => contactList.filter(contact => contact.id !== id))
-    }
+  const handleDelete = (id) => {
+    setContactList((contactList) => contactList.filter((contact) => contact.id !== id))
+  }
 
-   return (
-      <section className="contact-component">
-            <div className="container">
-                <div className="container__head">
-                    <div className="users-name">Welcome, {`${user.name} ${user.surname}`}</div>
-                    <Link type="submit" className="btn btn__contact" to='/login'>Log out</Link>
-                </div>
-                <Form
-                    onAddContact={handleAddContact}
-                />
+  return (
+    <section className="contact-component">
+      <div className="container">
+        <div className="container__head">
+          <div className="users-name">Welcome, {`${user.name} ${user.surname}`}</div>
+          <Link type="submit" className="btn btn__contact" to="/login">
+            Log out
+          </Link>
+        </div>
+        <Form onAddContact={handleAddContact} />
 
-                <ContactView
-                    contactList={contactList}
-                    onDeleteContact={handleDelete}
-                    onEditContact={handleEdit}
-                />
-            </div>
-      </section>
-   )
+        <ContactView
+          contactList={contactList}
+          onDeleteContact={handleDelete}
+          onEditContact={handleEdit}
+        />
+      </div>
+    </section>
+  )
 }
 
 export default Contact
--- src/component/ContactElement/index.jsx
+++ src/component/ContactElement/index.jsx
@@ -1,67 +1,64 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import './ContactElement.css'
 
-
 const ContactElement = ({ name, phone, id, onDelete, onEdit }) => {
-   const [isEditMode, setIsEditMode] = useState(false)
-   const [nameField, setNameField] = useState(name)
-   const [phoneField, setPhoneField] = useState(phone)
-   const firstInputElement = useRef(null)
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [nameField, setNameField] = useState(name)
+  const [phoneField, setPhoneField] = useState(phone)
+  const firstInputElement = useRef(null)
 
-   const toggleMode = () => {
-      setIsEditMode(isEditMode => !isEditMode)
-      if(!isEditMode) {
-         firstInputElement.current.disabled=false
-         firstInputElement.current.focus()
-      }
-   }
+  const toggleMode = () => {
+    setIsEditMode((isEditMode) => !isEditMode)
+    if (!isEditMode) {
+      firstInputElement.current.disabled = false
+      firstInputElement.current.focus()
+    }
+  }
 
-   const handleSave = () => {
-      onEdit(id, nameField, phoneField)
-      toggleMode()
-   }
+  const handleSave = () => {
+    onEdit(id, nameField, phoneField)
+    toggleMode()
+  }
 
-   return (
-      <section>
-         <div className="contact">
-            <div className="contact-text-and-edit">
-               <div className="contact-text">
-                  <input
-                      value={nameField}
-                      disabled={!isEditMode}
-                      ref={firstInputElement}
-                      onChange={(event) =>
-                          setNameField(event.target.value)}
-                  />
-                  <input
-                      value={phoneField}
-                      disabled={!isEditMode}
-                      onChange={(event) =>
-                          setPhoneField(event.target.value)}
-                  />
-               </div>
+  return (
+    <section>
+      <div className="contact">
+        <div className="contact-text-and-edit">
+          <div className="contact-text">
+            <input
+              value={nameField}
+              disabled={!isEditMode}
+              ref={firstInputElement}
+              onChange={(event) => setNameField(event.target.value)}
+            />
+            <input
+              value={phoneField}
+              disabled={!isEditMode}
+              onChange={(event) => setPhoneField(event.target.value)}
+            />
+          </div>
 
-               <button className="edit" onClick={isEditMode ? handleSave : toggleMode}>
-                  <i className="material-icons">{isEditMode ? "save" : "create"}</i>
-               </button>
-            </div>
+          <button className="edit" onClick={isEditMode ? handleSave : toggleMode}>
+            <i className="material-icons">{isEditMode ? 'save' : 'create'}</i>
+          </button>
+        </div>
 
-            <button className="delete" onClick={() => onDelete(id)}>
-               <i className="material-icons">clear</i>
-            </button>
-         </div>
-      </section>
-   )
+        <button className="delete" onClick={() => onDelete(id)}>
+          <i className="material-icons">clear</i>
+        </button>
+      </div>
+    </section>
+  )
 }
 
 ContactElement.propTypes = {
-   name: PropTypes.string.isRequired,
-   phone: PropTypes.number.isRequired,
-   id: PropTypes.number.isRequired,
-   onDelete: PropTypes.func.isRequired,
-   onEdit: PropTypes.func.isRequired
+  name: PropTypes.string.isRequired,
+  phone: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onEdit: PropTypes.func.isRequired,
 }
 
 export default ContactElement
